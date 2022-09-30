a70ca20482e36507afa3f2c161eb13a724e5fcc9
Hao Lee <110977525+hlee0122@users.noreply.github.com>
2022-09-30 13:35:56 +0100

    Update bracket_scaffold.py

--- examples/bracket_scaffold.py
+++ examples/bracket_scaffold.py
@@ -10,15 +10,15 @@ Meshing a mid-surfaced bracket for a structural analysis
 Objective
 ~~~~~~~~~
 
-To create conformal surface mesh, we will scaffold topofaces/topoedges to connect 
-all the surface bodies and mesh the bracket with quad elements. 
+To create conformal surface mesh, we will scaffold topofaces/topoedges to connect
+all the surface bodies and mesh the bracket with quad elements.
 
 
 Procedure
 ~~~~~~~~~
 * Launch Prime instance
 * Import CAD geometry and create part per CAD model
-* Scaffold topofaces and topoedges with tolerance parameter 
+* Scaffold topofaces and topoedges with tolerance parameter
 * Surface mesh topofaces with constant size and generate quad elements
 * Write a cdb file for use in the APDL solver
 * Exit the Prime session
@@ -46,15 +46,15 @@ bracket_file = prime.examples.download_file(r"bracket_mid_surface.scdoc")
 # Import geometry and create part per CAD model for topology based connection
 file_io = prime.FileIO(model)
 file_io.import_cad(
-    file_name=bracket_file, 
+    file_name=bracket_file,
     params=prime.ImportCadParams(
-        model=model, 
-        length_unit=prime.LengthUnit.MM, 
+        model=model,
+        length_unit=prime.LengthUnit.MM,
         part_creation_type=prime.PartCreationType.MODEL,
     )
 )
 
-# Get part summary 
+# Get part summary
 part = model.get_part_by_name('bracket_mid_surface')
 part_summary_res = part.get_summary(prime.PartSummaryParams(model, print_mesh=False))
 print(part_summary_res)
@@ -68,8 +68,8 @@ element_size = 0.5
 
 # Initialize connection tolerance and other parameters
 params = prime.ScaffolderParams(
-    model, absolute_dist_tol=0.1*element_size, 
-    intersection_control_mask=prime.IntersectionMask.FACEFACEANDEDGEEDGE, 
+    model, absolute_dist_tol=0.1*element_size,
+    intersection_control_mask=prime.IntersectionMask.FACEFACEANDEDGEEDGE,
     constant_mesh_size=element_size,
 )
 
@@ -79,8 +79,8 @@ beams = []
 
 # Scaffold topofaces and/or topoedges with connection parameters
 scaffold_res = prime.Scaffolder(model, part.id).scaffold_topo_faces_and_beams(
-    topo_faces=faces, 
-    topo_beams=beams, 
+    topo_faces=faces,
+    topo_beams=beams,
     params=params
 )
 print(scaffold_res)
@@ -91,9 +91,9 @@ print(scaffold_res)
 
 # Initialize surface meshing parameters
 surfer_params = prime.SurferParams(
-    model=model, 
+    model=model,
     size_field_type=prime.SizeFieldType.CONSTANT,
-    constant_size=element_size, 
+    constant_size=element_size,
     generate_quads=True,
 )
 
@@ -105,7 +105,7 @@ surfer_result = prime.Surfer(model).mesh_topo_faces(part.id, topo_faces=faces, p
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 export_res = file_io.export_mapdl_cdb(
-    os.path.join(os.getcwd(), "bracket_mid_surface.cdb"), 
+    os.path.join(os.getcwd(), "bracket_mid_surface.cdb"),
     prime.ExportMapdlCdbParams(model=model),
 )
 print(os.getcwd())
