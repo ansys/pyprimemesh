dbce3bc6f99eec753c9afa7fe338743a0b76275f
epeng <eric.peng@ansys.com>
2022-10-25 12:10:40 -0700

    test building doc

--- tests/common/runner.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from .result import PrimeTextTestResult
-
-
-class PrimeTextTestRunner(unittest.TextTestRunner):
-    resultClass = PrimeTextTestResult
-
-    def _makeResult(self):
-        return PrimeTextTestResult(self.stream, self.descriptions, self.verbosity)
