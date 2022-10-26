f06c4ef7d4bafa090ff2554bf495bd7e8c784222
epeng <eric.peng@ansys.com>
2022-10-26 11:05:46 -0700

    remove trailing white space

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -129,7 +129,7 @@ sphinx_gallery_conf = {
     "ignore_pattern": r"mixing_elbow",
     # Remove the "Download all examples" button from the top level gallery
     "download_all_examples": False,
-    # Sort gallery example by file name instead of number of lines (default) 
+    # Sort gallery example by file name instead of number of lines (default)
     "within_subsection_order": FileNameSortKey,
     # directory where function granular galleries are stored
     "backreferences_dir": None,
