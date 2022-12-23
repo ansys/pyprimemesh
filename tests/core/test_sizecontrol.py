5d9662c8e3713bc176e633f85a62cc0bb7478d50
afernand <alex.fernandezluces@ansys.com>
2022-12-23 11:41:25 +0100

    Improves test

--- tests/core/test_sizecontrol.py
+++ tests/core/test_sizecontrol.py
@@ -3,9 +3,11 @@ import ansys.meshing.prime as prime
 
 def test_sizecontrol(get_remote_client):
     model = get_remote_client.model
+    fileIO = prime.FileIO(model=model)
 
     size_control = prime.SizeControl(model, 0, 0, "foo")
     assert size_control.name == "foo"
 
     result = size_control.set_suggested_name('bar')
-    assert size_control.name == result.assigned_name
+    assert result.assigned_name != ''
+    assert result.assigned_name == size_control.name
