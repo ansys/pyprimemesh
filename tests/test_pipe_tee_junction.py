478a0ad7450423afccbdf9cbacacc609e3b14ea1
npawar <nikhilpawar428@gmail.com>
2022-12-23 12:20:31 +0530

    Resolving pre-commit-hook changes

--- tests/test_pipe_tee_junction.py
+++ tests/test_pipe_tee_junction.py
@@ -2,6 +2,7 @@ import math
 
 import ansys.meshing.prime as prime
 
+
 def test_pipe_tee_junction(get_remote_client):
     # downloads pmdat file
     pipe_tee = prime.examples.download_pipe_tee_pmdat()
@@ -17,19 +18,17 @@ def test_pipe_tee_junction(get_remote_client):
 
     mesher.create_zones_from_labels("outlet_main,in1_inlet,in2_inlet")
 
-    mesher.volume_mesh(prism_layers=5,
+    mesher.volume_mesh(
+        prism_layers=5,
         prism_surface_expression="* !*inlet* !*outlet*",
-        volume_fill_type=prime.VolumeFillType.POLY,
-    )
+        volume_fill_type=prime.VolumeFillType.POLY)
     # Get statistics on the mesh
     part = model.get_part_by_name("__wrap__")
     part_summary_res = part.get_summary(prime.PartSummaryParams(model=model))
-   
     # validate number of tri faces
     assert part_summary_res.n_tri_faces == 0
 
     # validate number of poly faces
     assert math.isclose(16717, float(part_summary_res.n_poly_faces), rel_tol=0.02)
-    # Style check fail with pre-commit
     # validate number of poly cells
     assert math.isclose(97341, float(part_summary_res.n_poly_cells), rel_tol=0.05)
