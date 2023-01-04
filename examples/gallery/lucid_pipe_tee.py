6d6ccc70dd646c803781f94b475e8b25ea66a6be
srpyprime <94401560+srpyprime@users.noreply.github.com>
2023-01-04 14:32:00 +0530

    Update lucid_pipe_tee.py

--- examples/gallery/lucid_pipe_tee.py
+++ examples/gallery/lucid_pipe_tee.py
@@ -12,7 +12,7 @@ and structural thermal simulation.
 Objective
 ~~~~~~~~~~
 
-In this example we will mesh the solids of a pipe T-section for a
+In this example, you can mesh the solids of a pipe T-section for a
 structural thermal analysis using tetrahedral elements and use the
 wrapper to extract the fluid domain and mesh using polyhedral cells with
 prismatic boundary layers.
@@ -39,9 +39,11 @@ Procedure
 ###############################################################################
 # Launch Ansys Prime Server
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Import all necessary modules.
-# Launch Ansys Prime Server instance and connect client.
-# Get the client model and instantiate meshing utilities from lucid class.
+#. Import all necessary modules.
+
+#. Launch Ansys Prime Server instance and connect client.
+
+#. Get the client model and instantiate meshing utilities from lucid class.
 
 from ansys.meshing import prime
 import ansys.meshing.prime.graphics as graphics
@@ -56,9 +58,12 @@ mesh_util = lucid.Mesh(model)
 ###############################################################################
 # Read CAD Geometry
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Download example FMD geometry file.
+#. Download example FMD geometry file.
+
 # FMD format is exported from SpaceClaim and is compatible with Linux.
-# Read and display the geometry file.
+
+#. Read and display the geometry file.
+
 # The file contains several unmeshed parts as you would get after you imported from CAD.
 
 file_name = prime.examples.download_pipe_tee_fmd()
@@ -72,11 +77,14 @@ print(model)
 ###############################################################################
 # Mesh for Structural
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Surface mesh using curvature sizing.
-# Volume mesh with tetrahedral elements.
-# Delete unwanted capping surface geometries by deleting
+#. Surface mesh using curvature sizing.
+
+#. Volume mesh with tetrahedral elements.
+
+#. Delete unwanted capping surface geometries by deleting
 # parts that do not have any volume zones.
-# Display structural thermal mesh ready for export.
+
+#. Display structural thermal mesh ready for export.
 
 mesh_util.surface_mesh(min_size=2.5, max_size=10)
 mesh_util.volume_mesh()
@@ -92,7 +100,7 @@ display()
 ###############################################################################
 # Write Structural Mesh
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Labels will be exported to CDB as collections for use in
+# Labels are exported to CDB as collections for 
 # applying load boundary conditions in the solver.
 
 struc_mesh = os.path.join(os.getcwd(), "pipe_tee.cdb")
@@ -103,22 +111,23 @@ print("\nExported Structural Mesh: ", struc_mesh)
 ###############################################################################
 # Extract Fluid by Wrapping
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# There is a small internal diameter change between flanges that
-# can be dealt with in several ways:
+# You can deal with the small internal diameter change between flanges in several ways:
 #
 #  * Connect the geometry to extract a volume and refine the
 #    mesh around this detail to capture.
 #  * Modify the geometry to remove the feature.
 #  * Wrap to extract the internal flow volume and walk over the feature.
 #
-# Here we will choose to wrap and walk over these features.
+# Here, you will choose to wrap and walk over these features.
 #
-# We will start by reading in the geometry again.
-# Next we will use a constant size wrap to walk over the diameter change
+#. Read in the geometry again.
+
+#. Use a constant size wrap to walk over the diameter change
 # feature and extract the largest internal volume as the fluid.
-# By default the wrap will use all parts as input and delete the input
+
+# By default, the wrap uses all parts as input and delete the input
 # geometry after wrapping unless keep_input is set as True.
-# When displaying we can avoid displaying unnecessary edge zones.
+# When displaying, you can avoid displaying unnecessary edge zones.
 
 mesh_util.read(file_name)
 
@@ -131,9 +140,11 @@ display()
 ###############################################################################
 # Volume Mesh Fluid
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Create zones for each label to be used for boundary conditions definitions.
-# Volume mesh with prism polyhedral not growing prisms from inlets and outlets.
-# Let us visualize the generated volume mesh.
+#. Create zones for each label to be used for boundary conditions definitions.
+
+#. Volume mesh with prism polyhedral not growing prisms from inlets and outlets.
+
+#. Visualize the generated volume mesh.
 # You can clearly see the prism layers that were specified by the Prism control.
 
 # set global sizing
