3bdfcda10f435187bebd2e5f023bd63fc9aff714
afernand <alex.fernandezluces@ansys.com>
2023-01-11 10:05:24 +0100

    Changes test to adapt to CI/CD

--- tests/core/test_sizecontrol.py
+++ tests/core/test_sizecontrol.py
@@ -3,8 +3,12 @@ import ansys.meshing.prime as prime
 
 def test_sizecontrol(get_remote_client):
     model = get_remote_client.model
-    util = prime.lucid.Mesh(model)
-    util.read(prime.examples.download_elbow_pmdat())
+    elbow_lucid = prime.examples.download_elbow_pmdat()
+    # reads file
+    model = get_remote_client.model
+    fileIO = prime.FileIO(model=model)
+    mesher = prime.lucid.Mesh(model)
+    mesher.read(file_name=elbow_lucid)
     size_control = model.control_data.create_size_control(prime.SizingType.CURVATURE)
     result = size_control.set_suggested_name('bar')
     assert result.assigned_name != ''
