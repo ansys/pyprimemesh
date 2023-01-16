1cd3491d02672bc796e5f60c9291a6be755e50e7
Ninad Kamat <ninad.kamat@ansys.com>
2023-01-16 12:02:25 -0600

    Update release version to 0.2.0

--- src/ansys/meshing/prime/_version.py
+++ src/ansys/meshing/prime/_version.py
@@ -16,7 +16,7 @@ Print the version
 """
 
 # major, minor, patch
-version_info = 0, 2, 0, 'dev18'
+version_info = 0, 2, 0
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
