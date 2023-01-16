d5ebed011fd142f7cb3863420788b024394d865d
Ninad Kamat <ninad.kamat@ansys.com>
2023-01-16 12:29:41 -0600

    Switch to 0.3.0.dev0

--- src/ansys/meshing/prime/_version.py
+++ src/ansys/meshing/prime/_version.py
@@ -16,7 +16,7 @@ Print the version
 """
 
 # major, minor, patch
-version_info = 0, 2, 0
+version_info = 0, 3, 0, 'dev0'
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
