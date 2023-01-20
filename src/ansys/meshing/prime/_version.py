9ddc6dc308b8a7e422d69335c1c88cc7d4d4edb0
Roberto Pastor Muela <roberto.pastormuela@ansys.com>
2023-01-20 08:31:09 +0100

    Moving setup.py to pyproject.toml

--- src/ansys/meshing/prime/_version.py
+++ src/ansys/meshing/prime/_version.py
@@ -1,9 +1,8 @@
 """Version of ansys-meshing-prime library.
 
 On the ``main`` branch, use 'devN' to denote a development version.
-For example:
 
-version_info = 0, 3, 0, 'dev0'
+Version number is modified in ``pyproject.toml`` file.
 
 Examples
 --------
@@ -15,8 +14,9 @@ Print the version
 
 """
 
-# major, minor, patch
-version_info = 0, 3, 0, 'dev0'
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:  # pragma: no cover
+    import importlib_metadata  # type: ignore
 
-# Nice string for the version
-__version__ = '.'.join(map(str, version_info))
+__version__ = importlib_metadata.version(__name__.replace(".", "-"))
