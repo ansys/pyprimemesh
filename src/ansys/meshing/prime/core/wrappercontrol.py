4e14f8e693aac7aad1b18b8b84f8fa56a1d39b8a
afernand <alex.fernandezluces@ansys.com>
2023-02-17 12:00:11 +0100

    Fix wrappercontroll pydocstyle

--- src/ansys/meshing/prime/core/wrappercontrol.py
+++ src/ansys/meshing/prime/core/wrappercontrol.py
@@ -1,3 +1,4 @@
+"""Module containing WrapperControl related classes and methods."""
 from ansys.meshing.prime.autogen.wrappercontrol import WrapperControl as _WrapperControl
 
 # isort: split
@@ -6,19 +7,39 @@ from ansys.meshing.prime.internals.comm_manager import CommunicationManager
 
 
 class WrapperControl(_WrapperControl):
-    """Wrapper Control to describe all parameters and controls used for wrapping."""
+    """Wrapper Control to describe all parameters and controls used for wrapping.
+
+    Parameters
+    ----------
+    model : CommunicationManager
+        Server model where to create and modify WrapperControls from.
+    id : int
+        ID of the control.
+    object_id : int
+        Object ID of the control.
+    name : str
+        Name of the WrapperControl
+    local : bool, optional
+        Unused, by default False
+    """
 
     def __init__(self, model: CommunicationManager, id: int, object_id: int, name: str):
-        """Initialize Part"""
+        """Initialize the superclass and Model variable."""
         self._model = model
         _WrapperControl.__init__(self, model, id, object_id, name)
 
     def __str__(self) -> str:
+        """Representation of the class in string format.
+
+        Returns
+        -------
+        str
+            Class data in string format.
+        """
         return "Not implemented yet"
 
     def set_suggested_name(self, name: str) -> SetNameResults:
-        """Sets the unique name for the wrapper control based on the given suggested name.
-
+        """Set the unique name for the wrapper control based on the given suggested name.
 
         Parameters
         ----------
@@ -42,7 +63,7 @@ class WrapperControl(_WrapperControl):
 
     @property
     def name(self):
-        """Gets the name of wrapper control.
+        """Get the name of wrapper control.
 
         Returns
         -------
