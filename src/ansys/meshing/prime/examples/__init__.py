4a5960b68e324e9fde0c0671b3f022f4c9a27ada
afernand <alex.fernandezluces@ansys.com>
2023-02-20 12:10:03 +0100

    Simplifies init imports

--- src/ansys/meshing/prime/examples/__init__.py
+++ src/ansys/meshing/prime/examples/__init__.py
@@ -1,13 +1,1 @@
-from .examples import (
-    clear_download_cache,
-    download_bracket_fmd,
-    download_bracket_scdoc,
-    download_elbow_fmd,
-    download_elbow_pmdat,
-    download_elbow_scdoc,
-    download_pipe_tee_fmd,
-    download_pipe_tee_pmdat,
-    download_toy_car_fmd,
-    download_toy_car_pmdat,
-    download_toy_car_scdoc,
-)
+from .examples import *
