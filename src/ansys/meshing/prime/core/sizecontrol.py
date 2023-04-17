6caff3dd83268866900a39d3d5fd0b65b252ff92
Alex Fernandez <21alex295@gmail.com>
2023-04-17 12:31:53 +0200

    PyDocStyle fixes (#383)
    Co-authored-by: Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>

--- src/ansys/meshing/prime/core/sizecontrol.py
+++ src/ansys/meshing/prime/core/sizecontrol.py
@@ -1,3 +1,4 @@
+"""Module containing SizeControl related classes and methods."""
 from ansys.meshing.prime.autogen.sizecontrol import SizeControl as _SizeControl
 
 # isort: split
@@ -6,22 +7,42 @@ from ansys.meshing.prime.autogen.sizecontrolstructs import SizeControlSummaryPar
 
 
 class SizeControl(_SizeControl):
-    """Size control is used to compute size field for volumetric surface meshing."""
+    """Compute size field for volumetric surface meshing.
+
+    Parameters
+    ----------
+    model : CommunicationManager
+        Server model where to create and modify SizeControls from.
+    id : int
+        ID of the control.
+    object_id : int
+        Object ID of the control.
+    name : str
+        Name of the SizeControl
+    local : bool, optional
+        Unused, by default False
+    """
 
     def __init__(self, model, id, object_id, name, local=False):
-        """__init__(Model self, int id, int object_id, char* name)"""
+        """Initialize class variables and superclass."""
         _SizeControl.__init__(self, model, id, object_id, name)
         self._model = model
         self._name = name
 
     def __str__(self) -> str:
+        """Representation of the class in string format.
+
+        Returns
+        -------
+        str
+            Class data in string format.
+        """
         params = SizeControlSummaryParams(model=self._model)
         result = _SizeControl.get_summary(self, params)
         return result.message
 
     def set_suggested_name(self, name: str) -> SetNameResults:
-        """Sets the unique name for the size control based on the given suggested name.
-
+        """Set the unique name for the size control based on the given suggested name.
 
         Parameters
         ----------
@@ -45,7 +66,7 @@ class SizeControl(_SizeControl):
 
     @property
     def name(self):
-        """Gets the name of size control.
+        """Get the name of size control.
 
         Returns
         -------
