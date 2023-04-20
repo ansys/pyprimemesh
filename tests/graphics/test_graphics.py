df73834f1725ff511aab96c3f64d3f867a9d7c0b
afernand <alex.fernandezluces@ansys.com>
2023-04-20 13:16:42 +0200

    Add integration test for the plotter

--- tests/graphics/test_graphics.py
+++ tests/graphics/test_graphics.py
@@ -8,11 +8,11 @@ IMAGE_RESULTS_DIR = "../../image_cache_dir/results/"
 
 
 def test_plotter(get_remote_client, get_examples, verify_image_cache):
-    bracket_file = get_examples["elbow_lucid"]
+    mixing_elbow = get_examples["elbow_lucid"]
     model = get_remote_client.model
     # Your code goes here...
     mesh_util = prime.lucid.Mesh(model=model)
-    mesh_util.read(bracket_file)
+    mesh_util.read(mixing_elbow)
 
     mesh_util.surface_mesh(min_size=5, max_size=20)
     mesh_util.volume_mesh(
@@ -21,8 +21,5 @@ def test_plotter(get_remote_client, get_examples, verify_image_cache):
         prism_layers=3,
     )
 
-    image_result_path = (
-        "C:\\Users\\afernand\\Documents\\repositories\\pyprimemesh\\image_cache_dir\\plot.png"
-    )
     display = Graphics(model)
     display()
