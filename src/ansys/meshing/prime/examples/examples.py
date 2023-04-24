55e97fe41dc2ec9a8c281e634dac7bb72aa78405
afernand <alex.fernandezluces@ansys.com>
2023-04-24 15:59:32 +0200

    Add examples to import list

--- src/ansys/meshing/prime/examples/examples.py
+++ src/ansys/meshing/prime/examples/examples.py
@@ -6,7 +6,28 @@ from typing import Optional, Union
 
 from .download_utilities import DownloadManager
 
-__all__ = ['clear_download_cache', 'get_file', 'Examples']
+__all__ = [
+    'get_file',
+    "download_bracket_dsco",
+    "download_bracket_fmd",
+    "download_bracket_scdoc",
+    "download_deformed_blade_dsco",
+    "download_deformed_blade_fmd",
+    "download_deformed_blade_scdoc",
+    "download_elbow_pmdat",
+    "download_elbow_fmd",
+    "download_elbow_scdoc",
+    "download_elbow_dsco",
+    "download_toy_car_fmd",
+    "download_toy_car_pmdat",
+    "download_toy_car_scdoc",
+    "download_toy_car_dsco",
+    "download_pipe_tee_fmd",
+    "download_pipe_tee_fmd",
+    "download_pipe_tee_dsco",
+    "download_toy_car_scdoc",
+    "download_pcb_pmdat",
+]
 
 
 class Examples(Enum):
