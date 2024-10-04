336e19585271db3876e5735e1a94e47a535191ac
Ravindra Chopade <ravindra.chopade@ansys.com>
2024-10-04 22:24:25 +0530

    fixed doc style issue

--- docker/build_docker_linux.py
+++ docker/build_docker_linux.py
@@ -20,50 +20,68 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import sys, os, fnmatch, json, subprocess, re, shutil, logging
+"""Script to build linux based docker image for the pyprimemesh project."""
+import json
+import logging
+import os
+import re
+import shutil
+import subprocess
+import sys
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # Check input
-if len( sys.argv ) < 2:
+if len(sys.argv) < 2:
     print("Please specify Ansys installation folder.")
     print("Example: {} /ansys_inc/v251".format(os.path.basename(__file__)))
     sys.exit(1)
-    
+
 AWP_ROOT = sys.argv[1]
 if not os.path.exists(AWP_ROOT):
     print("Could not find Ansys installation folder '{}'. Please try again.".format(AWP_ROOT))
     sys.exit(1)
-    
-#-------------------------------------------------------------------------------
-# Globals
-MANIFEST_SYMLINKS            = os.path.join(AWP_ROOT, "meshing", "Prime", "Manifest_PyPrimeSymlinks.json")
-MANIFEST_FOUND_IN_UNIFIED    = os.path.join(AWP_ROOT, "meshing", "Prime", "Manifest_AnsysInstallation.json")
-ASSEMBLED_PACKAGE_ROOT       = "PyPrimeMeshPackage"
-LOG_FILE                     = "AssemblePyPrimeMeshServerPackage.log"
 
-logging.basicConfig(filename=LOG_FILE, level=logging.DEBUG, filemode="w", format="{asctime} - {levelname} - {message}", style="{", datefmt="%Y-%m-%d %H:%M:%S")
+# -------------------------------------------------------------------------------
+# Globals
+MANIFEST_SYMLINKS = os.path.join(AWP_ROOT, "meshing", "Prime", "Manifest_PyPrimeSymlinks.json")
+MANIFEST_FOUND_IN_UNIFIED = os.path.join(
+    AWP_ROOT, "meshing", "Prime", "Manifest_AnsysInstallation.json"
+)
+ASSEMBLED_PACKAGE_ROOT = "PyPrimeMeshPackage"
+LOG_FILE = "AssemblePyPrimeMeshServerPackage.log"
+
+logging.basicConfig(
+    filename=LOG_FILE,
+    level=logging.DEBUG,
+    filemode="w",
+    format="{asctime} - {levelname} - {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 # logging.getLogger().addHandler(logging.StreamHandler(sys.stderr))
-            
-def load_manifest():   
-    # Load Ansys unified installation match manifest
+
+
+def load_manifest():
+    """Load Ansys unified installation match manifest."""
     with open(MANIFEST_FOUND_IN_UNIFIED, 'r') as file:
-        unifiedPathDict = json.load(file)  
+        unifiedPathDict = json.load(file)
 
     # Load symlink manifest
     with open(MANIFEST_SYMLINKS, 'r') as file:
-        allSymlinkDict = json.load(file)  
+        allSymlinkDict = json.load(file)
 
     return unifiedPathDict, allSymlinkDict
-   
+
+
 def create_symlink(link, target):
-    # Create symlinks as specified in PyPrimeMesh package manifest
+    """Create symlinks as specified in PyPrimeMesh package manifest."""
     cmd = ['ln', '-s', target, link]
     p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     (stdoutdata, stderrdata) = p.communicate()
-    
+
+
 def assemble_full_package(unifiedPathDict, allFileDict, dest_package_path):
-    # Assemble standalone PyPrimeMesh package using Ansys installation as source
-    
+    """Assemble standalone PyPrimeMesh package using Ansys installation as source."""
     if os.path.exists(dest_package_path):
         shutil.rmtree(dest_package_path, ignore_errors=True)
 
@@ -71,26 +89,34 @@ def assemble_full_package(unifiedPathDict, allFileDict, dest_package_path):
     for key, value in unifiedPathDict.items():
         source = os.path.join(AWP_ROOT, value["path"], key)
         target = os.path.join(dest_package_path, value["destination"])
-        
+
         if not os.path.exists(target):
             os.makedirs(target)
-            
+
         logging.info("\tcopying source {} --> target {}".format(source, target))
         shutil.copy(source, target)
 
     # Copy symlinks
-    for key, value in allSymlinkDict.items():        
+    for key, value in allSymlinkDict.items():
         dest_folder = re.sub(r'.*meshing/Prime', 'meshing/Prime', value["path"])
         symlink_folder = os.path.join(dest_package_path, dest_folder)
         try:
             os.chdir(symlink_folder)
-            cur_dir = os.getcwd()     
-            
-            if os.path.exists(value["target"]):            
-                logging.info("\tcreating symlink {}/{} --> target {}".format(cur_dir, value["link"], value["target"]))
+            cur_dir = os.getcwd()
+
+            if os.path.exists(value["target"]):
+                logging.info(
+                    "\tcreating symlink {}/{} --> target {}".format(
+                        cur_dir, value["link"], value["target"]
+                    )
+                )
                 create_symlink(value["link"], value["target"])
             else:
-                logging.warning("\tcould not create symlink {}/{} --> target {}. Target does not exist.".format(symlink_folder, value["link"], value["target"]))
+                logging.warning(
+                    "\tcould not create symlink {}/{} --> target {}. Target does not exist.".format(
+                        symlink_folder, value["link"], value["target"]
+                    )
+                )
         except:
             pass
     # TAR the temporary directory and delete it
@@ -102,15 +128,17 @@ def assemble_full_package(unifiedPathDict, allFileDict, dest_package_path):
     )
 
     # Move the TAR file to the docker directory
-    #print(">>> Moving ZIP file to temporary directory")
-    #shutil.move(tar_file, TMP_DIR)
+    # print(">>> Moving ZIP file to temporary directory")
+    # shutil.move(tar_file, TMP_DIR)
+
 
 def create_docker_image(dest_package_path):
+    """Create docker image from the archived package."""
     # Check if Docker is installed on the system
     print(">>> Checking if Docker is installed")
     if shutil.which("docker") is None:
         print("XXXXXXX Docker is not installed.. exiting process. XXXXXXX")
-        exit(0)
+        exit(1)
 
     # Build the docker image
     print(">>> Building docker image. This might take some time...")
@@ -119,8 +147,9 @@ def create_docker_image(dest_package_path):
         cwd=os.path.dirname(os.path.abspath(__file__)),
         capture_output=True,
     )
-    
-#-------------------------------------------------------------------------------
+
+
+# -------------------------------------------------------------------------------
 #
 # stand-alone use
 #
@@ -128,26 +157,36 @@ def create_docker_image(dest_package_path):
 if __name__ == "__main__":
 
     try:
-       
+
         # change into the directory where the script resides
         root = os.path.dirname(os.path.abspath(__file__))
         dest_package_path = os.path.join(root, ASSEMBLED_PACKAGE_ROOT)
         os.chdir(root)
-        
+
         unifiedPathDict, allSymlinkDict = load_manifest()
-        
+
         logging.info("Assembling Ansys PyPrimeMesh server package...")
         print("Assembling Ansys PyPrimeMesh server package...")
         assemble_full_package(unifiedPathDict, allSymlinkDict, dest_package_path)
 
-        logging.info("Processing complete. PyPrimeMesh server package assembled in {}.".format(dest_package_path))
-        print("Processing complete. \nPyPrimeMesh server package assembled in {}. \nDetails in log file {}.".format(dest_package_path, LOG_FILE))
+        logging.info(
+            "Processing complete. PyPrimeMesh server package assembled in {}.".format(
+                dest_package_path
+            )
+        )
+        print(
+            "Processing complete. \nPyPrimeMesh server package assembled in {}. "
+            "\nDetails in log file {}.".format(dest_package_path, LOG_FILE)
+        )
 
         create_docker_image(dest_package_path)
 
     except SystemExit:
         raise
     except Exception as msg:
-        logging.info(""" %s\n
-        An internal error occurred.""" % msg)        
+        logging.info(
+            """ %s\n
+        An internal error occurred."""
+            % msg
+        )
         sys.exit(1)
