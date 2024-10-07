6da4b75c383339ff566b4be55e627a15d3cb3648
Alex Fernandez <21alex295@gmail.com>
2024-10-07 11:50:21 +0200

    doc: Add quarto cheat sheet (#891)
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>
    Co-authored-by: Revathy Venugopal <104772255+Revathyvenugopal162@users.noreply.github.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -58,6 +58,11 @@ html_theme_options = {
             "icon": "fa fa-comment fa-fw",
         },
     ],
+    "cheatsheet": {
+        "file": "cheatsheet/cheat_sheet.qmd",
+        "title": "PyPrimeMesh cheat sheet",
+        "version": __version__,
+    },
 }
 
 # Sphinx extensions
