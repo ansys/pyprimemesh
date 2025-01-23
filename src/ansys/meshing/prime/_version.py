c8d8708d4d101542008635c97dca22333f5f6756
Subha Nayak <subha.nayak@ansys.com>
2025-01-23 22:15:34 +0530

    enabled copyright version

--- src/ansys/meshing/prime/_version.py
+++ src/ansys/meshing/prime/_version.py
@@ -1,22 +1,34 @@
-"""Version of ansys-meshing-prime library.
+# Copyright (C) 2024 - 2025 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-On the ``main`` branch, use 'dev0' to denote a development version.
-For example:
+"""Version of PyPrimeMesh Library."""
+# Version
+# ------------------------------------------------------------------------------
 
-version_info = 0, 1, 0, 'dev0'
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:  # pragma: no cover
+    import importlib_metadata  # type: ignore
 
-Examples
---------
-Print the version
+__version__ = importlib_metadata.version("ansys-meshing-prime")
 
->>> from ansys.product import library
->>> print(library.__version__)
-0.1.0.dev0
-
-"""
-
-# major, minor, patch
-version_info = 0, 8, 0
-
-# Nice string for the version
-__version__ = '.'.join(map(str, version_info))
+# ------------------------------------------------------------------------------
