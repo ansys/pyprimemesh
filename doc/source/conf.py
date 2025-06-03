94f8e756bbc2a0fc121a227edb550a95f8bce9da
Martin Walters <104021577+waltersma@users.noreply.github.com>
2025-06-03 11:32:40 +0100

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -183,5 +183,6 @@ supress_warnings = ["docutils"]
 autodoc_default_options = {    
     'members': True,    
     'undoc-members': True,    
-    'exclude-members': '__dict__, __weakref__, bit_count, to_bytes, from_bytes, as_integer_ratio, is_integer',
+    'exclude-members': '__dict__, __weakref__, bit_count, to_bytes, from_bytes, as_integer_ratio,
+is_integer',
 }
