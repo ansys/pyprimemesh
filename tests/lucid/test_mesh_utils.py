9052c26aabf8b69fcc012555a6920c31a810e65a
Alex Fernandez Luces <alex.fernandezluces@ansys.com>
2026-02-25 15:32:36 +0100

    test: Add test for from_geometry

--- tests/lucid/test_mesh_utils.py
+++ tests/lucid/test_mesh_utils.py
@@ -108,3 +108,19 @@ def test_delete_topo(mesh):
 def test_size_control(mesh):
     mesh.create_constant_size_control()
     mesh.create_curvature_size_control(max=1.0, min=2.0)
+
+
+def test_from_geometry(mesh):
+    from ansys.geometry.core import launch_modeler
+    from ansys.geometry.core.math import Point2D
+    from ansys.geometry.core.misc import UNITS
+    from ansys.geometry.core.sketch import Sketch
+    from pint import Quantity
+
+    modeler = launch_modeler()
+    design_name = "ExtrudeProfile"
+    design = modeler.create_design(design_name)
+    sketch = Sketch()
+    sketch.circle(Point2D([10, 10], UNITS.mm), Quantity(10, UNITS.mm))
+    body = design.extrude_sketch("SingleBody", sketch, Quantity(10, UNITS.mm))
+    mesh.from_geometry(body)
