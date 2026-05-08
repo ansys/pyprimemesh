948380d7fd403ec8ea4990c8178206cb50c255ca
Martin Walters <104021577+waltersma@users.noreply.github.com>
2026-05-08 11:37:19 +0100

    Update 04_lucid_toy_car.py

--- examples/gallery/04_lucid_toy_car.py
+++ examples/gallery/04_lucid_toy_car.py
@@ -56,7 +56,7 @@ Procedure
 
 """
 
-# sphinx_gallery_tags = ["Fluid", "Aerodynamics", "Tet", "Prism", "Inflation", "Wrap", "Diagnostics"]
+# sphinx_gallery_tags = ["Fluid", "Aerodynamics", "Tet", "Prism", "Wrap", "Diagnostics"]
 
 ###############################################################################
 # Launch Ansys Prime Server
