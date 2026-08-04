ab05ab7c4df2701cb7c681a82b12709d78218a8e
Martin Walters <104021577+waltersma@users.noreply.github.com>
2026-08-04 17:08:24 +0100

    Update __init__.py

--- src/ansys/meshing/prime/lucid/__init__.py
+++ src/ansys/meshing/prime/lucid/__init__.py
@@ -20,6 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-'''High level helper functions for PyPrimeMesh'''
+'''High level helper functions for PyPrimeMesh.'''
+
 from .mesh_util import LabelToZoneMethod, Mesh
 from .scope import SurfaceScope, VolumeScope
