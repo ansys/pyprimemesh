bb95b8f1b3a59894b9f5d977e2f2256efcdbaa01
Martin Walters <104021577+waltersma@users.noreply.github.com>
2026-08-21 21:26:20 +0100

    Update mesh_util.py

--- src/ansys/meshing/prime/lucid/mesh_util.py
+++ src/ansys/meshing/prime/lucid/mesh_util.py
@@ -70,6 +70,11 @@ class Mesh:
     * Helper method for creating zones from labels
     * Helper methods for reading and writing files
 
+    Parameters
+    ----------
+    model : prime.Model
+        Model that the methods are to work on.
+
     """
 
     def __init__(self, model: prime.Model):
@@ -97,7 +102,7 @@ class Mesh:
             Ansys Design object to import geometry from.
         import_type : ImportTypes, optional
             Type of import. The default is ImportTypes.FMD.
-        append: bool, optional
+        append : bool, optional
             Append imported CAD into existing model when true.
         """
         with tempfile.TemporaryDirectory() as tmpdir:
@@ -710,7 +715,7 @@ class Mesh:
             The default is ``"*"``.
         target_face_labels : str, optional
             Name pattern of the face labels to connect. The default is ``"*"``.
-        tolerance: float, optional
+        tolerance : float, optional
             Tolerance for the connection. The default is ``0.05``.
 
         """
@@ -1477,7 +1482,7 @@ class Mesh:
         use_existing_features : bool, optional
             Whether to maintain existing features on parts. The
             default is ``False``.
-        enable_feature_octree_refinement: bool, optional
+        enable_feature_octree_refinement : bool, optional
             Whether to apply refinement to feature edges during the wrap.
             The default is ``True``.
         feature_angle : float, optional
